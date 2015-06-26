fe7bad3e26d17e91e3cfb5ceda72607fae579ccb
Israel Hofman <icehofman@gmail.com>
2015-06-26 11:09:40 -0300

    First test.

--- WireMock/start.groovy
+++ WireMock/start.groovy
@@ -3,7 +3,7 @@
 import com.github.tomakehurst.wiremock.*
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
 
-def wireMockServer = new WireMockServer(wireMockConfig().port(8089)) //No-args constructor will start on port 8080, no HTTPS
+def wireMockServer = new WireMockServer(wireMockConfig().port(8089).withRootDirectory('/home/icehofman/AGCO/PocSoap/WireMock')) //No-args constructor will start on port 8080, no HTTPS
 wireMockServer.start()
 
 
