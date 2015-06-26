cd729c7d576974aa7bc8b264d1c635c0390c2094
Israel Hofman <icehofman@gmail.com>
2015-06-26 19:02:51 -0300

    Trying.

--- WireMock/start.groovy
+++ WireMock/start.groovy
@@ -1,11 +1,34 @@
 @Grab('com.github.tomakehurst:wiremock:1.56')
+@Grab('org.spockframework:spock-core:1.0-groovy-2.4')
 
-import com.github.tomakehurst.wiremock.*
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import spock.lang.Specification
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
 
-def wireMockServer = new WireMockServer(wireMockConfig().port(8089).withRootDirectory(new File(getClass().protectionDomain.codeSource.location.path).parent))
-wireMockServer.start()
+class WireMocking extends Specification {
+  def wireMockServer = new WireMockServer(wireMockConfig().port(8089).withRootDirectory(new File(getClass().protectionDomain.codeSource.location.path).parent))
+  def wireMock = new WireMock("localhost", 8089)
 
-//WireMock.reset()
+  def setup() {
+    wireMockServer.start()
+  }
 
-//wireMockServer.stop()
+  def cleanup() {
+    wireMockServer.stop()
+  }
+
+  def "Test01"() {
+    when:
+    wireMock.register(get(urlEqualTo("/test2"))
+    .willReturn(aResponse()
+    .withStatus(200)
+    .withBody("test02")
+    ))
+
+    then:
+    println wireMock
+  }
+}
