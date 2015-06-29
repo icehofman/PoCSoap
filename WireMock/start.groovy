7fadb08bc6fe83475d9ad3967266a8b64522e2d0
Israel Hofman <icehofman@gmail.com>
2015-06-29 18:21:14 -0300

    Testing things.

--- WireMock/start.groovy
+++ WireMock/start.groovy
@@ -1,5 +1,8 @@
 @Grab('com.github.tomakehurst:wiremock:1.56')
 @Grab('org.spockframework:spock-core:1.0-groovy-2.4')
+@Grab('com.github.groovy-wslite:groovy-wslite:1.1.2')
+
+import wslite.soap.*
 
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock
@@ -42,6 +45,20 @@ class WireMocking extends Specification {
     String html = 'http://localhost:8089/test2'.toURL().text
     println html
     assert 'test02' == html
+  }
+
+  def "Test03"() {
+    when:
+    def client = new SOAPClient('http://localhost:8089/test3')
+    def response = client.send() {
+      envelopeAttributes "stuff:inner":"http://foo.com/"
+      body{
+
+        }
+      }
+
+    then:
+    response
 
   }
 }
